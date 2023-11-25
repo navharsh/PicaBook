6d0330de5632bd55670bd984b321f58acb2d684c
Navharsh <navharshkumar12@gmail.com>
2023-11-26 05:04:32 +0530

    fix analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
+    <>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    <Analytics />
+    </>
   );
 }
