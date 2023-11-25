86d49999cd48d7f8f796a534a4d4db82691dfe04
Navharsh <navharshkumar12@gmail.com>
2023-11-26 04:19:58 +0530

    Testing Stories

--- components/Feed.js
+++ components/Feed.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+// import Story from "./Story";
+
 import WhatsOnYourMind from "./WhatsOnYourMind";
 import Post from "./Post";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
@@ -18,6 +20,7 @@ const Feed = () => {
 
   return (
     <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
+            {/* <Story /> */}
       <WhatsOnYourMind />
       {posts.map((post) => {
         return <Post key={post.id} id={post.id} data={post.data()} />;
